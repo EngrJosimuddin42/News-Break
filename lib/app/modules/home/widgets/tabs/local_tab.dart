c88347f0934489610848c20c4bc32c59ca24bdf4
Josimuddin <josimcse@gmail.com>
2026-04-22 08:57:07 +0600

    New Details screen

--- lib/app/modules/home/widgets/tabs/local_tab.dart
+++ lib/app/modules/home/widgets/tabs/local_tab.dart
@@ -21,181 +21,174 @@ class LocalTab extends GetView<HomeController> {
       if (controller.isLoading.value) {
         return const Center(child: CircularProgressIndicator());
       }
+      return _buildAuthAwareContent();
+    });
+  }
+
+  Widget _buildAuthAwareContent() {
+    return Obx(() {
       final loggedIn = AuthController.to.user.value != null;
       return loggedIn ? _buildLoggedIn() : _buildLoggedOut();
     });
   }
 
-  // Logged out
   Widget _buildLoggedOut() {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset('assets/images/socket.png',
-            width: 130,
-            height: 130,
-          ),
+          Image.asset('assets/images/socket.png', width: 130, height: 130),
           const SizedBox(height: 16),
-          Text(message,
-            style: AppTextStyles.caption.copyWith(color: Color(0xFF9B9B9B)),
-          ),
+          Text(message, style: AppTextStyles.caption.copyWith(color: const Color(0xFF9B9B9B))),
           const SizedBox(height: 16),
           OutlinedButton(
             onPressed: controller.onTryAgain,
             style: OutlinedButton.styleFrom(
-              padding: EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
               minimumSize: const Size(90, 50),
               side: const BorderSide(color: AppColors.linkColor),
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(60))),
-            child: Text('Try Again',
-              style: AppTextStyles.caption.copyWith(color: AppColors.linkColor),
-            ),
-          ),
+
+            child: Text('Try Again', style: AppTextStyles.caption.copyWith(color: AppColors.linkColor))),
         ],
       ),
     );
   }
 
-  // Logged in video news cards
   Widget _buildLoggedIn() {
     return ListView(
       padding: const EdgeInsets.only(top: 4, bottom: 16),
       children: [
-        // Weather section
         _buildWeatherSection(),
-
-        // News cards
-        Obx(() => Column(
-          children: controller.localNews.map((news) {
-            if (news.publisherType == 'Ad') {
-              return AdVideoCard(news: news);
-            }
-            return CategoryNewsCard(news: news);
-          }).toList(),
-        )),
+        _buildNewsCards(),
       ],
     );
   }
 
   Widget _buildWeatherSection() {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-      padding: const EdgeInsets.all(12),
-      child: Column(
-        children: [
-          // Temperature row
-          Row(
-            children: [
-             Obx(() => Text(controller.currentTemp.value, style: AppTextStyles.displaySmall)),
-              const SizedBox(width: 8),
-              Image.asset('assets/icons/weather_cloudy.png'),
-              SizedBox(width: 24),
-              // Forecast days
-              Flexible(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: controller.weatherDays.map((data) => Padding(
-                      padding: const EdgeInsets.only(left: 12),
-                      child: Column(
-                        children: [
-                          Text(data['day']!,
-                              style: AppTextStyles.display.copyWith(color: AppColors.textOnDark)),
-                          const SizedBox(height: 4),
-                          Image.asset(data['icon']!, width: 20, height: 20),
-                          const SizedBox(height: 4),
-                          Text(data['temp']!,
-                              style: AppTextStyles.display.copyWith(color: AppColors.textOnDark)),
-                        ],
-                      ),
-                    )).toList(),
+    return Obx(() {
+      final temp = controller.currentTemp.value;
+      final condition = controller.weatherCondition.value;
+      final days = controller.weatherDays.toList();
+
+      return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(temp, style: AppTextStyles.displaySmall),
+                const SizedBox(width: 8),
+                Image.asset('assets/icons/weather_cloudy.png'),
+                const SizedBox(width: 24),
+                Flexible(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: days.map((data) => Padding(
+                        padding: const EdgeInsets.only(left: 12),
+                        child: Column(
+                          children: [
+                            Text(data['day']!, style: AppTextStyles.display.copyWith(color: AppColors.textOnDark)),
+                            const SizedBox(height: 4),
+                            Image.asset(data['icon']!, width: 20, height: 20),
+                            const SizedBox(height: 4),
+                            Text(data['temp']!, style: AppTextStyles.display.copyWith(color: AppColors.textOnDark)),
+                          ],
+                        ),
+                      ))
+                          .toList(),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-
-          const SizedBox(height: 24),
-
-          // Rain expected button
-          GestureDetector(
-            onTap: () => _showRainForecast(),
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: 20, vertical: 12),
-              decoration: BoxDecoration(
-                border: Border.all(color: Color(0xFF808080)),
-                borderRadius: BorderRadius.circular(50),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset('assets/icons/weather_storm.png',color: AppColors.textOnDark,height: 20,width: 20),
-                  const SizedBox(width: 6),
-                  Obx((){
-                   return Text(
-                    '${controller.weatherCondition.value} expected',style: AppTextStyles.overline);}),
-                  const SizedBox(width: 6),
-                  Icon(Icons.chevron_right, color:AppColors.textOnDark, size: 20),
-                ],
+              ],
+            ),
+            const SizedBox(height: 24),
+            GestureDetector(
+              onTap: _showRainForecast,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xFF808080)),
+                  borderRadius: BorderRadius.circular(50)),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset('assets/icons/weather_storm.png', color: AppColors.textOnDark, height: 20, width: 20),
+                    const SizedBox(width: 6),
+                    Text('$condition expected', style: AppTextStyles.overline),
+                    const SizedBox(width: 6),
+                    const Icon(Icons.chevron_right, color: AppColors.textOnDark, size: 20),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    });
+  }
+
+  Widget _buildNewsCards() {
+    return Obx(() => Column(
+      children: controller.localNews.map((news) {
+        if (news.publisherType == 'Ad') return AdVideoCard(news: news);
+        return CategoryNewsCard(news: news);
+      }).toList(),
+    ));
   }
 
   void _showRainForecast() {
     showDialog(
       context: Get.context!,
-      builder: (_) =>
-          Dialog(
-            backgroundColor: const Color(0xFF252525),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20)),
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Close button
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: GestureDetector(
-                      onTap: () => Get.back(),
-                      child: const Icon(Icons.close,
-                          color: Colors.white, size: 20),
-                    ),
-                  ),
-                  SizedBox(height: 40),
-                  Text('Rain Forecast',
-                      style: AppTextStyles.button),
-                  const SizedBox(height: 6),
-                  Obx(() => Text('The probability of precipitation is ${controller.rainProbability.value}',
-                    style: AppTextStyles.labelMedium)),
-                  const SizedBox(height: 16),
+      builder: (_) => Dialog(
+        backgroundColor: const Color(0xFF252525),
+        shape:
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: GestureDetector(
+                  onTap: () => Get.back(),
+                  child:
+                  const Icon(Icons.close, color: Colors.white, size: 20))),
+              const SizedBox(height: 40),
+              Text('Rain Forecast', style: AppTextStyles.button),
+              const SizedBox(height: 6),
 
-                  // Chart
-                  Container(
-                    height: 250,
-                    padding: const EdgeInsets.fromLTRB(20, 70, 20, 50),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: const Color(0xFFEBEBEB)),
-                      borderRadius: BorderRadius.circular(14),
+
+              Obx(() {
+                final probability = controller.rainProbability.value;
+                final barData = controller.rainBarData.toList();
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('The probability of precipitation is $probability', style: AppTextStyles.labelMedium),
+                    const SizedBox(height: 16),
+                    Container(height: 250,
+                      padding: const EdgeInsets.fromLTRB(20, 70, 20, 50),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: const Color(0xFFEBEBEB)),
+                        borderRadius: BorderRadius.circular(14)),
+                      child: CustomPaint(
+                        size: Size.infinite,
+                        painter: _RainChartPainter(data: barData),
+                      ),
                     ),
-                    child: Obx(() => CustomPaint(
-                      size: Size.infinite,
-                      painter: _RainChartPainter(data: controller.rainBarData),
-                    )),
-                  ),
-                ],
-              ),
-            ),
+                  ],
+                );
+              }),
+            ],
           ),
+        ),
+      ),
     );
   }
 }
@@ -242,18 +235,25 @@ class _RainChartPainter extends CustomPainter {
     }
 
     // X-axis Labels (Now, 10PM...)
+    final labelBarIndices = [0, 2, 4, data.length - 1];
     final labels = ['Now', '10 PM', '04 AM', '10 AM'];
     final labelStyle = TextStyle(color: Colors.white38, fontSize: 10);
+
     for (int i = 0; i < labels.length; i++) {
-      final xPos = i * (size.width / (labels.length - 1));
+      final barIndex = labelBarIndices[i];
+
+      final double barCenterX = (barIndex * spacing) + (spacing / 2);
+
       final tp = TextPainter(
-          text: TextSpan(text: labels[i], style: labelStyle),
-          textDirection: TextDirection.ltr)
-        ..layout();
-      double xOffset = xPos - (tp.width / 2);
-      if (i == 0) xOffset = 0;
-      if (i == labels.length - 1) xOffset = size.width - tp.width;
-      tp.paint(canvas, Offset(xOffset, size.height + 10));
+        text: TextSpan(text: labels[i], style: labelStyle),
+        textDirection: TextDirection.ltr,
+      )..layout();
+
+      double xOffset = barCenterX - (tp.width / 2);
+
+      xOffset = xOffset.clamp(0.0, size.width - tp.width);
+
+      tp.paint(canvas, Offset(xOffset, size.height + 14));
     }
 
 
@@ -264,8 +264,7 @@ class _RainChartPainter extends CustomPainter {
     yLabels.forEach((text, yPos) {
       final tp = TextPainter(
         text: TextSpan(text: text, style: yStyle),
-        textDirection: TextDirection.ltr,
-      )
+        textDirection: TextDirection.ltr)
         ..layout();
 
       if (text == 'in') {
@@ -278,9 +277,7 @@ class _RainChartPainter extends CustomPainter {
     // Date label on tallest bar
     final dateLabel = '04/02';
     final dateTp = TextPainter(
-        text: TextSpan(
-            text: dateLabel,
-            style: const TextStyle(color: Colors.white, fontSize: 9)),
+        text: TextSpan(text: dateLabel, style: const TextStyle(color: Colors.white, fontSize: 9)),
         textDirection: TextDirection.ltr)
       ..layout();
     final tallestIndex = 4; // 0.08 value
@@ -302,8 +299,7 @@ class _RainChartPainter extends CustomPainter {
       canvas.drawLine(
         start + direction * drawn,
         start + direction * (dashEnd < distance ? dashEnd : distance),
-        paint,
-      );
+        paint);
       drawn += dashWidth + dashSpace;
     }
   }
